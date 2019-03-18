879d6f4d61754d5afb046cc4b0c6c5917e5139b2
marcelo <marceloestudoifes@gmail.com>
2019-03-18 15:04:14 +0000

    att

--- POO/projetov2/View/index.php
+++ POO/projetov2/View/index.php
@@ -1,47 +1,9 @@
-<!DOCTYPE html>
-<html>
- <head>
-   <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
-   <link rel="icon" href="../assets/favicon.ico" type="image/x-icon">
-    <title>banco de horas</title>
-    
- </head>
-<style>
-   body{
-      margin:0px;
-   }
-   header{
-      position:relative;
-      width:100%;
-      height:100px;
-   }
-   header img{
-      position:relative;
-      margin:10px;
-   }
-   header label{
-      color:#3232ff;
-      position:relative;
-      top:-100px;
-      left:100px;
-   }
-</style>
- <body>
-   
-   <header>
-         <img src="../assets/logo" alt="Logo" height="80" width="80">
-       <label>
-            <h1>Banco de horas</h1>
-        </label>
-   </header>
-
-   
-
-
-
-
-
-
-
- </body>
-</html>
+<?php 
+    if(isset($_SESSION['login'])){
+        // go to page 
+    }
+    else{
+        // go to login
+        header('Location: login.php');
+    }
+?>
