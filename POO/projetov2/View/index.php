fd0ff19f4cc29d24085914635499b2ba9c2207d2
marcelo <marceloestudoifes@gmail.com>
2019-03-18 13:15:04 +0000

    config

--- POO/projetov2/View/index.php
+++ POO/projetov2/View/index.php
@@ -1,19 +1,47 @@
 <!DOCTYPE html>
 <html>
  <head>
+   <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
+   <link rel="icon" href="../assets/favicon.ico" type="image/x-icon">
     <title>banco de horas</title>
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    
  </head>
 <style>
-
-
+   body{
+      margin:0px;
+   }
+   header{
+      position:relative;
+      width:100%;
+      height:100px;
+   }
+   header img{
+      position:relative;
+      margin:10px;
+   }
+   header label{
+      color:#3232ff;
+      position:relative;
+      top:-100px;
+      left:100px;
+   }
 </style>
  <body>
-    <header>
-        <img src="assets/logo.png">    
+   
+   <header>
+         <img src="../assets/logo" alt="Logo" height="80" width="80">
        <label>
-            <h2>Banco de horas</h2>
+            <h1>Banco de horas</h1>
         </label>
-    </header>
+   </header>
+
+   
+
+
+
+
+
+
+
  </body>
 </html>
